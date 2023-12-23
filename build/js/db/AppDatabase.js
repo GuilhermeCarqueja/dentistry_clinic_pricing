4227621cb8d02fe46664130b7c4f3d84291ff5e8
andreprovensi <andreprovensi@gmail.com>
2023-12-23 20:05:57 -0300

    adding typescript

---
+++ build/js/db/AppDatabase.js
@@ -0,0 +1,33 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sqlite3 = require('sqlite3').verbose();
+class AppDatabase {
+    constructor() {
+        this.READ_ONLY_MODE = sqlite3.OPEN_READONLY;
+        this.READ_WRITE_MODE = sqlite3.OPEN_READWRITE;
+        this.DB_URL = './database.sqlite';
+    }
+    connectToDataBaseReadOnly() {
+        return new Promise((resolve, reject) => {
+            try {
+                const db = new sqlite3.Database(this.DB_URL, this.READ_ONLY_MODE, (err) => reject(err));
+                resolve(db);
+            }
+            catch (error) {
+                reject(error);
+            }
+        });
+    }
+    connectToDataBaseReadAndWrite() {
+        return new Promise((resolve, reject) => {
+            try {
+                const db = new sqlite3.Database(this.DB_URL, this.READ_WRITE_MODE, (err) => reject(err));
+                resolve(db);
+            }
+            catch (error) {
+                reject(error);
+            }
+        });
+    }
+}
+module.exports = AppDatabase;
---
+++ src/db/AppDatabase.ts
@@ -0,0 +1,40 @@
+import { Database, sqlite3 } from "sqlite3";
+
+const sqlite3:sqlite3 = require('sqlite3').verbose()
+
+class AppDatabase {
+
+    private readonly READ_ONLY_MODE:number;
+    private readonly READ_WRITE_MODE:number;
+    private DB_URL:string
+
+    constructor(){
+        this.READ_ONLY_MODE =  sqlite3.OPEN_READONLY;
+        this.READ_WRITE_MODE = sqlite3.OPEN_READWRITE;
+        this.DB_URL = './database.sqlite';
+    }
+
+    public connectToDataBaseReadOnly(): Promise<Database>{
+        return new Promise((resolve,reject)=>{
+            try {
+                const db = new sqlite3.Database(this.DB_URL,this.READ_ONLY_MODE, (err:Error|null):void=>reject(err));
+                resolve(db)
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
+
+    public connectToDataBaseReadAndWrite(): Promise<Database>{
+        return new Promise((resolve,reject)=>{
+            try {
+                const db = new sqlite3.Database(this.DB_URL,this.READ_WRITE_MODE ,(err:Error|null)=>reject(err));
+                resolve(db)
+            } catch (error) {
+                reject(error)
+            }
+        })
+    }
+}
+
+module.exports = AppDatabase
