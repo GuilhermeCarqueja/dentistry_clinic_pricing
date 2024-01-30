bce098bd4c4f8c7917b50a4bd8bab68dfe25fee3
andreprovensi <andreprovensi@gmail.com>
2024-01-30 20:58:10 -0300

    updateing CREATE TABLES queries

--- src/db/database_init.ts
+++ src/db/database_init.ts
@@ -15,7 +15,7 @@ const appDatabase = new AppDatabase();
 const QUERY_CREATE_USERS_TABLE: string = `
     CREATE TABLE IF NOT EXISTS 
     users (
-        user_id INT PRIMARY KEY,
+        user_id INTEGER PRIMARY KEY,
         user_street VARCHAR(200) NOT NULL,
         user_address_numer INTEGER NOT NULL,
         user_email VARCHAR(100) NOT NULL,
@@ -30,14 +30,15 @@ const QUERY_CREATE_USERS_TABLE: string = `
 const QUERY_CREATE_CLINICS_TABLE: string = `
     CREATE TABLE IF NOT EXISTS 
     clinics (
-        clinic_id INT PRIMARY KEY,
-        max_payment_fee FLOAT NOT NULL,
-        tax_rate FLOAT NOT NULL,
-        number_of_daily_work_hours INT NOT NULL,
+        clinic_id INTEGER PRIMARY KEY,
+        max_payment_fee FLOAT,
+        tax_rate FLOAT,
+        number_of_daily_work_hours INTEGER NOT NULL,
         clinic_name VARCHAR (255) NOT NULL,
-        number_of_chairs INT NO NULL,
+        number_of_chairs INTEGER NO NULL,
         number_of_work_months FLOAT NOT NULL,
-        user_id INT,
+        expected_vacancy FLOAT,
+        user_id INTEGER,
         iserted_at TEXT NOT NULL,
         FOREIGN KEY (user_id) REFERENCES users(user_id)
     )
@@ -46,10 +47,10 @@ const QUERY_CREATE_CLINICS_TABLE: string = `
 const QUERY_CREATE_RECURRENT_EXPENSES_TABLE: string = `
 CREATE TABLE IF NOT EXISTS 
 clinic_current_expenses (
-    expense_id INT PRIMARY KEY,
+    expense_id INTEGER PRIMARY KEY,
     expense VARCHAR(200) NOT NULL,
-    clinic_id INT,
-    expense_value DOUBLE NOT NULL,
+    clinic_id INTEGER NOT NULL,
+    expense_value FLOAT,
     expense_frequency CHARACTER(1) NOT NULL,
     inserted_at TEXT NOT NULL,
     FOREIGN KEY(clinic_id) REFERENCES clinics(clinic_id)
@@ -58,9 +59,9 @@ clinic_current_expenses (
 const QUERY_CREATE_PROCEDURES_LIST_TABLE: string = `
 CREATE TABLE IF NOT EXISTS 
 procedures_list (
-    list_id INT PRIMARY KEY,
+    list_id INTEGER PRIMARY KEY,
     list_name VARCHAR(200) NOT NULL,
-    clinic_id INT,
+    clinic_id INTEGER NOT NULL,
     inserted_at TEXT NOT NULL,
     FOREIGN KEY(clinic_id) REFERENCES clinics(clinic_id)
     )
@@ -68,33 +69,34 @@ procedures_list (
         
 const QUERY_CREATE_PROCEDURES_TABLE: string = `
 CREATE TABLE IF NOT EXISTS 
-procedures (
-    procedure_id INT PRIMARY KEY,
-    investment_fee FLOAT NOT NULL,
-    procedure_tax FLOAT NOT NULL,
-    minimum_price FLOAT NOT NULL,
-    professional_cost FLOAT NOT NULL,
-    profit_margin FLOAT NOT NULL,
-    precedure_max_payment_fee FLOAT NOT NULL,
-    procedure_time FLOAT NOT NULL,
-    procedure_time_cost FLOAT NO NULL,
-    list_id INT,
-    procedure_name VARCHAR(200) NOT NULL,
-    final_price FLOAT NOT NULL,
-    materials_cost NOT NULL,
-    inserted_at TEXT NOT NULL,
-    FOREIGN KEY(list_id) REFERENCES procedures_list(list_id)
-    )
+    procedures (
+        procedure_id INTEGER PRIMARY KEY,
+        investment_fee FLOAT,
+        procedure_tax FLOAT,
+        minimum_price FLOAT,
+        professional_cost FLOAT,
+        profit_margin FLOAT,
+        precedure_max_payment_fee FLOAT,
+        precedure_max_payment_cost FLOAT,
+        procedure_time FLOAT,
+        procedure_time_cost FLOAT,
+        list_id INTEGER,
+        procedure_name VARCHAR(200) NOT NULL,
+        final_price FLOAT NOT NULL,
+        materials_cost NOT NULL,
+        inserted_at TEXT NOT NULL,
+        FOREIGN KEY(list_id) REFERENCES procedures_list(list_id)
+        )
     `
 
 const QUERY_CREATE_MATERIALS_TABLE: string = `
 CREATE TABLE IF NOT EXISTS 
 materials (
-    material_id INT PRIMARY KEY,
+    material_id INTEGER PRIMARY KEY,
     unit VARCHAR(10) NOT NULL,
     purchase_quantity FLOAT NOT NULL,
     purchase_cost float NOT NULL,
-    clinic_id INT,
+    clinic_id INTEGER,
     inserted_at TEXT NOT NULL,
     FOREIGN KEY(clinic_id) REFERENCES clinics(clinic_id)
     )
@@ -103,8 +105,8 @@ materials (
 const QUERY_CREATE_PROCEDURE_MATERIALS_TABLE: string = `
 CREATE TABLE IF NOT EXISTS 
 procedure_materials (
-    procedure_id INT,
-    material_id INT,
+    procedure_id INTEGER NOT NULL,
+    material_id INTEGER NOT NULL,
     unit VARCHAR(10) NOT NULL,
     inserted_at TEXT NOT NULL,     
     FOREIGN KEY(material_id) REFERENCES materials(material_id),
